3b6ec2d4967fa337c1b78632156082921f25f6da
Harini Maruthasalam <114240304+HariniMaruthasalam@users.noreply.github.com>
2024-04-07 13:44:38 +0530

    Create Check_Array_Sorted.java

---
+++ Check_Array_Sorted.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int i = 0, flag = 0, n = nums.length;
+        boolean status = true;
+        while(i<n-1){
+            if(flag == 0){
+                if(nums[i] > nums[i+1]){
+                    flag = 1;
+                }
+            }else{
+                if(nums[0] < nums[i] || nums[i] > nums[i+1])                 {
+                    status = false;
+                    break;
+                }
+            }
+            i++;
+        }
+        if(flag == 1 && nums[0] < nums[i]) status = false;
+        return status;
+    }
+}
